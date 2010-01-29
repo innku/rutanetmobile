ba7137d8a20f09ad9dcb09fe544d9c7c45a809ef
Federico Gonzalez <federico@innku.com>
2010-01-29 16:14:27 -0600

    Tabs con cargas y busqueda y detalles de diseño

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,10 @@
 require 'rho/rhoapplication'
 
 class AppApplication < Rho::RhoApplication
+  def initialize
+    @tabs = [{ :label => "Buscar", :action => '/app/Freight/new', :icon => "/public/images/search.png", :reload => true }, 
+             { :label => "Mis Cargas", :action => '/app/Contact', :icon => "/public/images/truck.png", :reload => true },
+             { :label => "Configuracion", :action => '/app/Settings', :icon => "/public/images/settings.png", :reload => true }]
+    super
+  end
 end
