ad550b050188bf16796476bf2b9ea3d9f064401d
Federico Gonzalez <federico@innku.com>
2010-02-15 14:33:57 -0600

    Ignorar log y build

--- app/application.rb
+++ app/application.rb
@@ -4,6 +4,7 @@ class AppApplication < Rho::RhoApplication
   def initialize
     @tabs = [{ :label => "Buscar", :action => '/app/Freight/new', :icon => "/public/images/search.png", :reload => true }, 
              { :label => "Cargas", :action => '/app/Freight/index', :icon => "/public/images/truck.png", :reload => true },
+             { :label => "Contactos", :action => '/app/Contact/index', :icon => "/public/images/truck.png", :reload => true },
              { :label => "Configuracion", :action => '/app/Settings', :icon => "/public/images/settings.png", :reload => true }]
     super
   end
