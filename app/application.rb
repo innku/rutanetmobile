40f11037dbcfb34666786e87562c2513c277a65d
Federico Gonzalez <federico@innku.com>
2010-02-16 11:41:09 -0600

    Quitar extra tab en iphone

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,6 @@ class AppApplication < Rho::RhoApplication
   def initialize
     @tabs = [{ :label => "Buscar", :action => '/app/Freight/new', :icon => "/public/images/search.png", :reload => true }, 
              { :label => "Mis Cargas", :action => '/app/Myfreight/index', :icon => "/public/images/truck.png", :reload => true },
-             { :label => "Contactos", :action => '/app/Contact/index', :icon => "/public/images/truck.png", :reload => true },
              { :label => "Configuracion", :action => '/app/Settings', :icon => "/public/images/settings.png", :reload => true }]
     super
   end
