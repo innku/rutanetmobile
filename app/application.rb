a9fc55bb41187bd78eb90d449df8446a164774e3
Adrian Cuadros <adrian@innku.com>
2010-02-16 12:26:17 -0600

    Se agrega el link de back

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,8 @@ require 'rho/rhoapplication'
 class AppApplication < Rho::RhoApplication
   def initialize
     @tabs = [{ :label => "Buscar", :action => '/app/Freight/new', :icon => "/public/images/search.png", :reload => true }, 
-             { :label => "Mis Cargas", :action => '/app/Myfreight/index', :icon => "/public/images/truck.png", :reload => true },
-             { :label => "Configuracion", :action => '/app/Settings', :icon => "/public/images/settings.png", :reload => true }]
+             { :label => "Mis Cargas", :action => '/app/Myfreight/index', :icon => "/public/images/truck.png", :reload => true }]
+          #{ :label => "Configuracion", :action => '/app/Settings', :icon => "/public/images/settings.png", :reload => true }]
     super
   end
 end
